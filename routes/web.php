71b5929415baf30dbe10d2d5a43da61798b13dcf
Alberto Staboli <albertostaboli@gmail.com>
2025-10-20 20:08:08 +0200

    Push del mio bel progetto Laravel

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/welcome', function (){
+    return view('welcome');
+});
+Route::get('/pagina1', function (){
+    return view('pagina1');
+});
+Route::get('/pagina2', function (){
+    return view('pagina2');
+});
